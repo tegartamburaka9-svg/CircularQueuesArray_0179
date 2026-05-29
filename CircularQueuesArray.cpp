83f1c296ed26c34890efce1a7d4da8e90501a6bf
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-29 16:37:23 +0700

    membuat void remove

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -47,4 +47,21 @@ public:
         queue_array[REAR] = num;
     }
 
-    
+    void remove()
+    {
+        // ccek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT]
+            << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+       
