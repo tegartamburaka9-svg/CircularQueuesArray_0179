69a03d6bc2fff4a580d8eda8132d6f623d5efc97
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-29 16:42:23 +0700

    membuat case 3

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -151,4 +151,9 @@ int main()
                 q.remove();
                 break;
             }
+            case '3':
+            {
+                q.display();
+                break;
+            }
             
