7a2f0221834d777e83738a9dbbaffa3b086fbc16
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-29 16:34:43 +0700

    membuat if untuk mengecek antrian

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -23,4 +23,17 @@ public:
         cin >> num;
         cout << endl;
 
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
         
